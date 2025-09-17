0d11576c220900b756a5c6713eda9eae6b4c0dc2
Akash Jaiswal <akashjaiswal3846@gmail.com>
2025-09-17 14:01:28 +0530

    test
    Signed-off-by: Akash Jaiswal <akashjaiswal3846@gmail.com>

--- hack/install-tools.sh
+++ hack/install-tools.sh
@@ -15,24 +15,23 @@ sudo apt-get install -y build-essential make git curl wget apt-transport-https c
 # -------------------------------
 # Install NVIDIA Container Toolkit
 # -------------------------------
-curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
-  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
-    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+# curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+#   && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+#     sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+#     sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
-sudo apt-get update -y
+# sudo apt-get update -y
 
-export NVIDIA_CONTAINER_TOOLKIT_VERSION=1.17.8-1
-sudo apt-get install -y \
-    nvidia-container-toolkit=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
-    nvidia-container-toolkit-base=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
-    libnvidia-container-tools=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
-    libnvidia-container1=${NVIDIA_CONTAINER_TOOLKIT_VERSION}
+# export NVIDIA_CONTAINER_TOOLKIT_VERSION=1.17.8-1
+# sudo apt-get install -y \
+#     nvidia-container-toolkit=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+#     nvidia-container-toolkit-base=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+#     libnvidia-container-tools=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+#     libnvidia-container1=${NVIDIA_CONTAINER_TOOLKIT_VERSION}
 
 # Install NVIDIA Driver
-sudo apt update
-sudo apt install nvidia-driver-535 -y
-sudo apt install nvidia-utils-535
+# sudo apt update
+# sudo apt install nvidia-driver-535 -y
 
 ## Privilaged access for docker to make sure Kind can access GPU resources
 alias docker="sudo docker"
